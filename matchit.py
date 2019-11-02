a8245fb326079025a8babf27f220601660470cc4
Thy Nguyen ( pronouns as tee: win) <thynguyen2511@gmail.com>
2019-11-02 15:16:06 -0700

    Update matchit.py

--- matchit.py
+++ matchit.py
@@ -41,11 +41,23 @@ class MatchGame(object):
     NUM_GRIDS = 4
     CANVAS_SIZE = SQUARE_SIZE * NUM_GRIDS + 100
     score = 100
-    count = 0
+    num_of_tries = 0
     pic = []
+    default_color = 'yellow'
 
     def __init__(self, parent, player_color, folder, delay):
         parent.title('Match it!')
+        self.color = player_color
+        self.delay = delay
+        self.folder = folder
+        self.image_id = []
+        # Initiate list of images
+        self.new_list = get_image_list(folder) * 2
+        random.shuffle(self.new_list)
+        # Initialize self.sammy
+        self.sammy = tkinter.PhotoImage(file=os.path.join(self.folder,
+                                                          self.new_list[0]))
+        print(self.new_list)  # to check the 16 images in random order
         # Create the restart button widget
         restart_btn = tkinter.Button(parent, text='RESTART', width=20,
                                      command=self.restart)
@@ -53,32 +65,16 @@ class MatchGame(object):
         # Create a canvas widget
         self.canvas = tkinter.Canvas(parent, width=self.CANVAS_SIZE,
                                      height=self.CANVAS_SIZE)
+        # create the grid on canvas
+        self.create_grid()
         self.canvas.grid()
-
-        self.new_list = get_image_list(folder) * 2
-        random.shuffle(self.new_list)
-        print(self.new_list)
-
-        # create the grid frame on canvas
-        self.image_id = []
-        for y_iter in range(self.NUM_GRIDS):
-            for x_iter in range(self.NUM_GRIDS):
-                tile = tkinter.Canvas(self.canvas, width=self.SQUARE_SIZE,
-                                      height=self.SQUARE_SIZE,
-                                      background='yellow')
-                tile.create_rectangle(0, 0, self.SQUARE_SIZE, self.SQUARE_SIZE,
-                                      outline='black')
-                self.pic = tkinter.PhotoImage(
-                    file=os.path.join(folder, self.new_list[x_iter]))
-                id = tile.create_image(self.SQUARE_SIZE / 2,
-                                                  self.SQUARE_SIZE / 2,
-                                                  image=self.pic)
-                self.image_id.append(id)
-                tile.grid(column=x_iter, row=y_iter)
-        # self.create_grids(self.canvas)
-        self.color = player_color
-        self.delay = delay
-
+        # self.pic = tkinter.PhotoImage(
+        #     file=os.path.join(folder, self.new_list[x_iter]))
+        # img_id = self.canvas.create_image(self.SQUARE_SIZE / 2,
+        #                                   self.SQUARE_SIZE / 2,
+        #                                   image=self.pic)
+        # self.image_id.append(img_id)
+        self.canvas.bind("<Button-1>", self.play)
         # Create a label widget for the score and end of game messages
         self.score_label = tkinter.Label(parent, text=f'Score: {self.score}',
                                          borderwidth=1)
@@ -93,7 +89,7 @@ class MatchGame(object):
         :return: None
         """
         self.score = 100
-        self.count = 0
+        self.num_of_tries = 0
         # random.shuffle(get_image_list(folder))
 
     def play(self, event):
@@ -103,54 +99,52 @@ class MatchGame(object):
         :param event: event (Event object) describing the click event
         :return: None
         """
-        pass  # take out the pass statement and enter your code
+        tile = self.canvas.find_closest(event.x, event.y)
+        tile_id = tile[0] - 1
+        print(tile)  # tile id, eg (1,) when click the first square
+        # self.select(tile)  # to create the tag for tile
+        cords = self.canvas.coords(tile)  # coordinates of tile
+        print(os.path.join(self.folder, self.new_list[tile_id]))
+        self.sammy = tkinter.PhotoImage(file=os.path.join(self.folder,
+                                        self.new_list[tile_id]))
+        print(cords)
+        self.image_id = self.canvas.create_image((cords[0] + cords[2])/2,
+                                                 (cords[1] + cords[3])/2,
+                                                 image=self.sammy)
+        self.canvas.after(self.delay, self.disappear, tile)
 
-    # def create_grids(self, parent):
-    #
-    #     for y_iter in range(self.NUM_GRIDS):
-    #         for x_iter in range(self.NUM_GRIDS):
-    #             tile = tkinter.Canvas(parent, width=self.SQUARE_SIZE,
-    #                                   height=self.SQUARE_SIZE,
-    #                                   background='yellow')
-    #             tile.create_rectangle(0, 0, self.SQUARE_SIZE, self.SQUARE_SIZE,
-    #                                   outline='black')
-    #             tile.grid(column=x_iter, row=y_iter)
+    def create_grid(self):
+        for y_iter in range(self.NUM_GRIDS):
+            for x_iter in range(self.NUM_GRIDS):
+                x, y = x_iter * self.SQUARE_SIZE, y_iter * self.SQUARE_SIZE
+                x_stop, y_stop = x + self.SQUARE_SIZE, y + self.SQUARE_SIZE
+                self.canvas.create_rectangle(x, y, x_stop, y_stop,
+                                             outline=self.color,
+                                             fill=self.default_color)
 
-    def blue(self):
-        self.color = 'blue'
-
-    def green(self):
-        self.color = 'green'
-
-    def magenta(self):
-        self.color = 'magneta'
-
-    def disappear(self):
+    def disappear(self, tile):
         """
         Remove Sammy's image from the Canvas
         Call appear to have the image reappear after a delay
         :return: None
         """
         self.canvas.delete(self.image_id)
-        self.canvas.after(1000, self.appear)
+        self.canvas.itemconfigure(tile, fill=self.default_color)
 
-    def appear(self):
+    def select(self, tile):
         """
-        Add Sammy's image to Canvas
-        Call disappear to have the image disappear after a delay
+        Tag the clicked tile as selected/unselected
+        :param tile:
         :return: None
         """
-        self.image_id = self.canvas.create_image(150, 150,
-                                                 image=self.sammy)
-        self.canvas.after(1000, self.disappear)
+        if "selected" in self.canvas.gettags(tile):
+            # print("unselected")
+            self.canvas.itemconfigure(tile, tag="unselected")
+        else:
+            # print("selected")
+            self.canvas.itemconfigure(tile, tag="selected")
 
-    # Enter your additional method definitions below
-    # Make sure they are indented inside the MatchGame class
-    # Make sure you include docstrings for all the methods.
 
-
-# Enter any function definitions here to get and validate the
-# command line arguments.  Include docstrings.
 def get_arguments():
     """
     Parse and validate the command line arguments
@@ -171,14 +165,17 @@ def get_arguments():
     return color, image_folder, fast
 
 
-def file_type(folder):
+def file_type(dir):
     """
     Validate if the folder exists and contains at least 8 gif files
     :param folder:
     :return:
     """
+    path, folder = os.path.split(dir)
+    if path == '':
+        path = '.'
     # check if the folder is in the directory
-    if folder not in os.listdir():
+    if folder not in os.listdir(path):
         raise argparse.ArgumentTypeError(f'{folder} is not a valid folder')
     else:
         # check if it contains at least 8 gif files
@@ -199,17 +196,18 @@ def get_image_list(folder):
 
 def main():
     # Retrieve and validate the command line arguments using argparse
-    delay_time = 1
+    delay_time = 3000
     color, image_folder, fast = get_arguments()
     if fast:
-        delay_time = 3
+        delay_time = 1000
     # Instantiate a root window
     root = tkinter.Tk()
     # Instantiate a MatchGame object with the correct arguments
-    match_game = MatchGame(root, color, image_folder, delay_time)
+    MatchGame(root, color, image_folder, delay_time)
     # Enter the main event loop
     root.mainloop()
 
 
 if __name__ == '__main__':
     main()
+
