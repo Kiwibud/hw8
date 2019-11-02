5b307de88553952e3503a566b19fca78dc63176a
Grace To <tps.sang@gmail.com>
2019-11-02 01:36:02 -0700

    Update matchit.py

--- matchit.py
+++ matchit.py
@@ -20,8 +20,6 @@ import tkinter
 import os
 import random
 import argparse
-import sys
-
 
 
 class MatchGame(object):
@@ -44,29 +42,48 @@ class MatchGame(object):
     CANVAS_SIZE = SQUARE_SIZE * NUM_GRIDS + 100
     score = 100
     count = 0
+    pic = []
 
     def __init__(self, parent, player_color, folder, delay):
         parent.title('Match it!')
         # Create the restart button widget
         restart_btn = tkinter.Button(parent, text='RESTART', width=20,
-                                 command=self.restart)
+                                     command=self.restart)
         restart_btn.grid()
         # Create a canvas widget
         self.canvas = tkinter.Canvas(parent, width=self.CANVAS_SIZE,
                                      height=self.CANVAS_SIZE)
-        for i in self.canvas.find_all():
-            self.pic = tkinter.PhotoImage(file=get_image_list(folder)[0])
-            self.image_id = self.canvas.create_image(50, 50, image=self.pic)
         self.canvas.grid()
+
+        self.new_list = get_image_list(folder) * 2
+        random.shuffle(self.new_list)
+        print(self.new_list)
+
         # create the grid frame on canvas
-        self.create_grids(self.canvas)
+        self.image_id = []
+        for y_iter in range(self.NUM_GRIDS):
+            for x_iter in range(self.NUM_GRIDS):
+                tile = tkinter.Canvas(self.canvas, width=self.SQUARE_SIZE,
+                                      height=self.SQUARE_SIZE,
+                                      background='yellow')
+                tile.create_rectangle(0, 0, self.SQUARE_SIZE, self.SQUARE_SIZE,
+                                      outline='black')
+                self.pic = tkinter.PhotoImage(
+                    file=os.path.join(folder, self.new_list[x_iter]))
+                id = tile.create_image(self.SQUARE_SIZE / 2,
+                                                  self.SQUARE_SIZE / 2,
+                                                  image=self.pic)
+                self.image_id.append(id)
+                tile.grid(column=x_iter, row=y_iter)
+        # self.create_grids(self.canvas)
+        self.color = player_color
+        self.delay = delay
+
         # Create a label widget for the score and end of game messages
         self.score_label = tkinter.Label(parent, text=f'Score: {self.score}',
-                                   borderwidth=1)
+                                         borderwidth=1)
         self.score_label.grid()
         # Create any additional instance variable you need for the game
-
-
 
     def restart(self):
         """
@@ -79,7 +96,6 @@ class MatchGame(object):
         self.count = 0
         # random.shuffle(get_image_list(folder))
 
-
     def play(self, event):
         """
         This method is invoked when the user clicks on a square.
@@ -89,14 +105,44 @@ class MatchGame(object):
         """
         pass  # take out the pass statement and enter your code
 
-    def create_grids(self, parent):
-        for y_iter in range(self.NUM_GRIDS):
-            for x_iter in range(self.NUM_GRIDS):
-                tile = tkinter.Canvas(parent, width=self.SQUARE_SIZE,
-                                      height=self.SQUARE_SIZE)
-                tile.create_rectangle(0, 0, self.SQUARE_SIZE, self.SQUARE_SIZE,
-                                      outline='black', fill='yellow')
-                tile.grid(column=x_iter, row=y_iter)
+    # def create_grids(self, parent):
+    #
+    #     for y_iter in range(self.NUM_GRIDS):
+    #         for x_iter in range(self.NUM_GRIDS):
+    #             tile = tkinter.Canvas(parent, width=self.SQUARE_SIZE,
+    #                                   height=self.SQUARE_SIZE,
+    #                                   background='yellow')
+    #             tile.create_rectangle(0, 0, self.SQUARE_SIZE, self.SQUARE_SIZE,
+    #                                   outline='black')
+    #             tile.grid(column=x_iter, row=y_iter)
+
+    def blue(self):
+        self.color = 'blue'
+
+    def green(self):
+        self.color = 'green'
+
+    def magenta(self):
+        self.color = 'magneta'
+
+    def disappear(self):
+        """
+        Remove Sammy's image from the Canvas
+        Call appear to have the image reappear after a delay
+        :return: None
+        """
+        self.canvas.delete(self.image_id)
+        self.canvas.after(1000, self.appear)
+
+    def appear(self):
+        """
+        Add Sammy's image to Canvas
+        Call disappear to have the image disappear after a delay
+        :return: None
+        """
+        self.image_id = self.canvas.create_image(150, 150,
+                                                 image=self.sammy)
+        self.canvas.after(1000, self.disappear)
 
     # Enter your additional method definitions below
     # Make sure they are indented inside the MatchGame class
